f2bc86b98a293e4222310834f4760d9db2cc6cd7
Aykan Örsçelik <44747491+aykan2004@users.noreply.github.com>
2025-11-23 18:51:23 -0500

    Task 1.1

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,21 @@
 package theater;
 
-/**
- * Class representing a performance of a play..
- */
-public class Performance {
+@SuppressWarnings({"checkstyle:WriteTag", "checkstyle:SuppressWarnings"})
+public final class Performance {
 
-    public String playID;
-    public int audience;
+    private final String playID;
+    private final int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
+    }
+
+    public String getPlayID() {
+        return playID;
+    }
+
+    public int getAudience() {
+        return audience;
     }
 }
